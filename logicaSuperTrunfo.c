251108f2c7f6d948545d77354bcaba7ed54ab5a1
Lucas <lucas.s.augusto@hotmail.com>
2025-10-09 10:20:13 -0300

    Nivel aventureiro completo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,6 +2,9 @@
 #include <string.h>
 
 int main() {
+    // Variaveis de utilidade
+    int userOption;
+
     // Dados da primeira carta
 
     char state_1, code_1[5], city_1[50];
@@ -78,19 +81,96 @@ int main() {
 
     pibCapita_2 = (float) pib_2 / populationAmount_2; // PIB per Capita
 
-    // Fazendo a comparação - Atributo escolhido: Densidade Populacional
+    // Fazendo a comparação
 
-    printf("\nComparação das cartas (Atributo: Densidade Populacional - Menor Vence) \n\n");
-    printf("Carta 1 - %s: %.2f \n", city_1, populationDensity_1);
-    printf("Carta 2 - %s: %.2f \n", city_2, populationDensity_2);
+    // Menu interativo
+    printf("Atributos comparaveis:\n");
+    printf("1 - População\n");
+    printf("2 - Area\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turisticos\n");
+    printf("5 - Densidade Demografica\n");
+    printf("Escolha o atributo que você deseja comparar.\n");
+    scanf("%d", &userOption);
 
-    // Como o vencedor desse atributo o menor vence, fiz uso do sinal de comparação menor (<) e aproveitei a oportunidade para implementar o caso de empate
-    if (populationDensity_1 < populationDensity_2) {
-        printf("A cidade vencedora é %s", city_1);
-    } else if (populationDensity_2 < populationDensity_1) {
-        printf("A cidade vencedora é %s", city_2);
-    } else {
-        printf("Empate!");
+    printf("%s VS %s\n", city_1, city_2);
+
+    // Aplicando o switch para a escolha de caminhos alternativos de acordo com o atributo
+    switch (userOption)
+    {
+    case 1:
+        printf("Atributo - População\n");
+        printf("%s possui %d pessoas.\n", city_1, populationAmount_1);
+        printf("%s possui %d pessoas.\n", city_2, populationAmount_2);
+
+        if (populationAmount_1 > populationAmount_2) {
+            printf("Primeira cidade venceu! (%s)", city_1);
+        } else if (populationAmount_1 < populationAmount_2) {
+            printf("Segunda cidade venceu! (%s)", city_2);
+        } else {
+            printf("Empate!");
+        }
+
+        break;
+    case 2:
+        printf("Atributo - Area\n");
+        printf("%s possui uma area de %.2f.\n", city_1, area_1);
+        printf("%s possui uma area de %.2f.\n", city_2, area_2);
+
+        if (area_1 > area_2) {
+            printf("Primeira cidade venceu! (%s)", city_1);
+        } else if (area_1 < area_2) {
+            printf("Segunda cidade venceu! (%s)", city_2);
+        } else {
+            printf("Empate!");
+        }
+
+        break;
+    case 3:
+        printf("Atributo - PIB\n");
+        printf("%s possui um PIB de %.2f.\n", city_1, pib_1);
+        printf("%s possui um PIB de %.2f.\n", city_2, pib_2);
+
+        if (pib_1 > pib_2) {
+            printf("Primeira cidade venceu! (%s)", city_1);
+        } else if (pib_1 < pib_2) {
+            printf("Segunda cidade venceu! (%s)", city_2);
+        } else {
+            printf("Empate!");
+        }
+
+        break;
+    case 4:
+        printf("Atributo - Pontos Turisticos\n");
+        printf("%s tem um total de %d pontos turisticos.\n", city_1, sights_1);
+        printf("%s tem um total de %d pontos turisticos.\n", city_2, sights_2);
+
+        if (sights_1 > sights_2) {
+            printf("Primeira cidade venceu! (%s)", city_1);
+        } else if (sights_1 < sights_2) {
+            printf("Segunda cidade venceu! (%s)", city_2);
+        } else {
+            printf("Empate!");
+        }
+
+        break;
+    case 5:
+        printf("Atributo - Densidade Populacional\n");
+        printf("%s tem uma densidade populacional de %.2f habitantes.\n", city_1, populationDensity_1);
+        printf("%s tem uma densidade populacional de %.2f habitantes.\n", city_2, populationDensity_2);
+
+        if (populationDensity_1 > populationDensity_2) {
+            printf("Segunda cidade venceu! (%s)", city_2);
+        } else if (populationDensity_1 < populationDensity_2) {
+            printf("Primeira cidade venceu! (%s)", city_1);
+        } else {
+            printf("Empate!");
+        }
+
+        break;
+    default:
+        printf("Opção inválida");
+        break;
     }
 
     return 0;
